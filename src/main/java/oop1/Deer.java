319472f574f00a02a986ecb461ff5516b46e9be2
kanon-nyc <57508437+kanon-nyc@users.noreply.github.com>
2021-03-08 17:29:58 -0500

    First commit

---
+++ src/main/java/oop1/Deer.java
@@ -0,0 +1,16 @@
+package oop1;
+
+public class Deer {
+    public void a(){
+
+    }
+    //constructor only be the same name of the class name.
+    public  void  Deer(){
+
+    }
+    Deer deer = new Deer();
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
